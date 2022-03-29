11f6fc43dc46cc1f2b1516fbdd66270ed96daad6
mcnamara-charles <c.w.mcnamara@vikes.csuohio.edu>
2022-03-29 05:06:34 -0400

    Removed unused modules

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,7 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var webpackConfig = {
   mode: 'production',
   entry: {
-    advanced_table: './src/visualizations/advanced_table/advanced_table.js',
     v1_common: './src/common/common-entry.js',
-    liquid_fill_gauge: './src/visualizations/liquid_fill_gauge/liquid_fill_gauge.ts',
-    sunburst: './src/visualizations/sunburst/sunburst.ts',
-    chord: './src/visualizations/chord/chord.ts',
-    treemap: './src/visualizations/treemap/treemap.ts',
-    image_carousel: './src/visualizations/image_carousel/image_carousel.js',
     d3_test: './src/visualizations/tests/custom-d3-test.js',
     custom_companies_table: './src/visualizations/custom_companies_table/custom_companies_table.js',
     custom_intent_table: './src/visualizations/custom_intent_table/custom_intent_table.js',
