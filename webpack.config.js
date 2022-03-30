1d440e0dc8fd37ad24d184848dade31c9f121bcf
mcnamara-charles <c.w.mcnamara@vikes.csuohio.edu>
2022-03-30 09:58:09 -0400

    Created unified bar chart

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,9 @@ var webpackConfig = {
     custom_intent_table: './src/visualizations/custom_intent_table/custom_intent_table.js',
     custom_companies_synopsis: './src/visualizations/custom_companies_synopsis/custom_companies_synopsis.js',
     custom_intent_synopsis: './src/visualizations/custom_intent_synopsis/custom_intent_synopsis.js',
-    custom_company_headcount: './src/visualizations/company_headcount/company_headcount.js',
+    custom_company_headcount: './src/visualizations/company_headcount/h-bar.js',
+    custom_intent_topic: './src/visualizations/custom_intent_topic/v-bar.js',
+    horizontal_bar: './src/visualizations/bar_charts/h-bar.js',
   },
   output: {
     filename: "[name].js",
