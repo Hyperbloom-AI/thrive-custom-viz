b46e95fbb57951f40f07bc15b215d58f5068a584
mcnamara-charles <c.w.mcnamara@vikes.csuohio.edu>
2022-05-27 12:10:24 -0400

    Made changes to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,12 @@ var webpackConfig = {
   mode: 'production',
   entry: {
     v1_common: './src/common/common-entry.js',
-    d3_test: './src/visualizations/tests/custom-d3-test.js',
     custom_companies_table: './src/visualizations/custom_companies_table/custom_companies_table.js',
     custom_intent_table: './src/visualizations/custom_intent_table/custom_intent_table.js',
+    firebolt_intent_table: './src/visualizations/custom_intent_table/custom_intent_table_firebolt.js',
     custom_companies_synopsis: './src/visualizations/custom_companies_synopsis/custom_companies_synopsis.js',
     custom_intent_synopsis: './src/visualizations/custom_intent_synopsis/custom_intent_synopsis.js',
+    firebolt_intent_synopsis: './src/visualizations/custom_intent_synopsis/custom_intent_synopsis_firebolt.js',
     horizontal_bar: './src/visualizations/bar_charts/h-bar.js',
     vertical_bar: './src/visualizations/bar_charts/v-bar.js',
   },
